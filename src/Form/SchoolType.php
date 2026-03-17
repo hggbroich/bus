bcb856b4ce0e051a578a036399c3ffcfac96e932
Marcel Marnitz <marcelmarnitz@googlemail.com>
2026-03-17 08:32:31 +0100

    Remove own school from SchoolType

--- src/Form/SchoolType.php
+++ src/Form/SchoolType.php
@@ -2,19 +2,41 @@
 
 namespace App\Form;
 
-use App\Entity\Country;
 use App\Entity\School;
+use App\Settings\OrderSettings;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SchoolType extends EntityType {
+
+
+    public function __construct(
+        public readonly OrderSettings $orderSettings,
+        ManagerRegistry $registry
+    ) {
+        parent::__construct($registry);
+    }
+
     public function configureOptions(OptionsResolver $resolver): void {
         parent::configureOptions($resolver);
 
         $resolver->setDefault('label', 'label.school');
         $resolver->setDefault('class', School::class);
-        //$resolver->setDefault('choice_label', 'name');
         $resolver->setDefault('multiple', false);
         $resolver->setDefault('expanded', false);
+        $resolver->setDefault('query_builder', function(EntityRepository $repository): QueryBuilder {
+            $qb = $repository->createQueryBuilder('s')
+                ->orderBy('s.name', 'ASC');
+
+            if($this->orderSettings->school !== null) {
+                $qb->andWhere('s.id != :school')
+                    ->setParameter('school', $this->orderSettings->school->getId());
+            }
+
+            return $qb;
+        });
     }
 }
